5495c5c623f4aead6b45251d9ce1355feb64f4a4
CotNeo <120303878+CotNeo@users.noreply.github.com>
2025-02-20 03:11:43 +0300

    Fix GitHub Pages deployment

--- Part_2/phonebook/vite.config.js
+++ Part_2/phonebook/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/phonebook-app/",
+  base: "/fullstack-Open/phonebook/", // GitHub Pages alt dizini için gerekli
   plugins: [react()],
 });
